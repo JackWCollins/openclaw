0a48b6d757044f96c48828fa3554e851f5312803
Jack Collins <hello@jackcollins.io>
2026-03-02 23:21:36 -0700

    Include config file in render-entrypoint.sh
    Added configuration file argument to the entrypoint script.

--- render-entrypoint.sh
+++ render-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-
 PORT="${PORT:-18789}"
 
 exec node /app/openclaw.mjs gateway \
   --allow-unconfigured \
   --port "$PORT" \
-  --bind lan
+  --bind lan \
+  --config /app/openclaw.config.json
